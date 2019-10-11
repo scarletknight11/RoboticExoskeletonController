368f36b571046f373a8ca4bc4607878380f97b20
Sanjit Singh <sanjitpsingh@yahoo.com>
2019-10-10 22:27:20 -0400

    Leap motion detection works

---
+++ Assets/Scripts/SphereGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereGenerator : MonoBehaviour {
+
+    public void CreateFallingSphere()
+    {
+        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        go.transform.position = new Vector3(0, 10, 0);
+        go.AddComponent<Rigidbody>();
+    }
+}
